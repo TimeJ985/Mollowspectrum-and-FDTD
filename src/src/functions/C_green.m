1ffc51d93b423de76c24ee0b68e2314da8e5d619
TimeJ985 <felix.hitzelhammer@uni-graz.at>
2025-12-12 16:24:07 +0000

    Delete src/src/functions directory

--- src/src/functions/C_green.m
+++
@@ -1,6 +0,0 @@
-function C1 = C_green(Cov, A, tout)
-    % greens functions propagator 
-    Ccell = arrayfun(@(t) expm(A * t) * Cov, tout, 'UniformOutput', false);
-
-    C1 = cat(3, Ccell{:});
-end
